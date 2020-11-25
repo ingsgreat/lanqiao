098b60e30e3becd711a2e2f8d9d07dab956fc5d8
ingsgreat <1209455541@qq.com>
2020-11-25 21:02:29 +0800

    xunlain

---
+++ 2020.11.251/AI/src/AI.java
@@ -0,0 +1,92 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import java.io.*;
+public class AI extends MIDlet
+{
+	Display display;
+	MainCanvas mc;
+	public AI(){
+		display=Display.getDisplay(this);
+		mc=new MainCanvas();
+		display.setCurrent(mc);
+	}
+	public void startApp(){
+	}
+	public void destroyApp(boolean unc){
+	}
+	public void pauseApp(){
+	}
+}
+class MainCanvas extends Canvas
+{
+	Image currentImg;
+	Image heroImg[][]=new Image[4][3];
+	int x,y;
+	int leftFlag,rightFlag,upFlag,downFlag;
+	public MainCanvas(){
+	    try
+	    {
+			for (int i=0;i<heroImg.length ; i++){
+				for(int j=0;j<heroImg[i].length;j++)
+				heroImg[i][j]=Image.createImage("/sayo"+i+j+".png");
+			}
+			
+			currentImg=heroImg[3][1];
+			x=120;
+			y=100;
+			leftFlag=1;
+		}
+		catch (IOException e){
+			e.printStackTrace();
+	    }
+	}
+	public void paint(Graphics g){
+		g.setColor(250,200,180);
+		g.fillRect(0,0,getWidth(),getHeight());
+		g.drawImage(currentImg,120,100,0);
+	}
+	public void keyPressed(int keyCode){
+		int action=getGameAction(keyCode);
+		if(action==LEFT){
+			if(leftFlag==1){
+				currentImg=heroImg[0][0];
+				leftFlag++;
+			}else if(leftFlag==2){
+				currentImg=heroImg[0][2];
+				leftFlag=1;
+			}
+			x=x-1;
+		}
+		if(action==RIGHT){
+			if(rightFlag==1){
+				currentImg=heroImg[1][0];
+				rightFlag++;
+			}else if(rightFlag==2){
+				currentImg=heroImg[1][2];
+				rightFlag=1;
+			}
+			x=x+1;
+		}
+		if(action==UP){
+			if(upFlag==1){
+				currentImg=heroImg[2][0];
+				upFlag++;
+			}else if(upFlag==2){
+				currentImg=heroImg[2][2];
+				upFlag=1;
+			}
+			y=y-1;
+		}
+		if(action==DOWN){
+			if(downFlag==1){
+				currentImg=heroImg[3][0];
+				downFlag++;
+			}else if(downFlag==2){
+				currentImg=heroImg[3][2];
+				downFlag=1;
+			}
+			y=y+1;
+		}
+		repaint();
+	}
+}
